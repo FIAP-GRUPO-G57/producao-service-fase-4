04788431d0cab866c77e2e506ebcb069e285e1fb
Gian Gomes <g_gomees@hotmail.com>
2024-05-20 23:37:01 -0300

    Refatorar para gradle

--- src/main/java/br/com/fiap/lanchonete/ProducaoServiceFase4Application.java
+++ src/main/java/br/com/fiap/lanchonete/ProducaoServiceFase4Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LanchoneteApplication {
+public class ProducaoServiceFase4Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LanchoneteApplication.class, args);
+		SpringApplication.run(ProducaoServiceFase4Application.class, args);
 	}
 
 }
